2f20919f6afa96e928f628e20de4f99e127f01d7
smatand <andrejsmatana0@gmail.com>
2022-02-10 11:48:24 +0100

    added comments in ppm.h and steg-decode.c

--- ppm.h
+++ ppm.h
@@ -1,7 +1,8 @@
 #ifndef PPM_H
 #define PPM_H
 
-#define MAX_IMAGE_SIZE (8000 * 8000 * 3) // 192m
+/* maximum size of ppm image */
+#define MAX_IMAGE_SIZE (8000 * 8000 * 3) // 192m										
 #define MAX_RGB 255
 
 
@@ -11,7 +12,21 @@ struct ppm {
 	char data[]; // RGB bytes, in total (3 * xsize * ysize)
 };
 
+/**
+ * reads from file filename and stores elements of created struct ppm
+ * ppm->data is dynamically allocated, so it must have its free() fcn
+ *
+ * @param filename used to read the file from
+ *
+ * @return struct with defined sizes and dynamically alloc-ted char[]
+ */
 struct ppm * ppm_read(const char * filename);
+
+/**
+ * frees dynamically allocated struct ppm
+ *
+ * @param *p used to free from memory
+ */
 void ppm_free(struct ppm *p);
 
 #endif // PPM_H
--- steg-decode.c
+++ steg-decode.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,41 +7,53 @@
 #include "error.h"
 
 int main(int argc, char* argv[]) {
+	// program must have 1 argument with name of file to open
 	if (argc != 2) {
 		error_exit("Program musi prijat jenom jeden argument s nazvem souboru ke cteni!");	
 	}
 	// get ppm data
-	struct ppm *image = ppm_read(argv[1]); 	
+	struct ppm *image= ppm_read(argv[1]); 	
+	// set the size of ppm, so the bitset array will be created
 	unsigned size = image->xsize * image->ysize * 3;
 
-	// get prime number indexes of bytes from which the LSB bit will be read to assembly a char
 	bitset_alloc(primes, size) 
-	Eratosthenes(primes);
-	// according to assignment, we may print the string only if the '\0' char is found
-	char * toPrint = malloc(1); // sizeof(char)
-//	char toPrint = '\0'; // in case of NULL the program will end with SUCCESS
-	int count = 0;
-	int index = 0;
+	Eratosthenes(primes); // get prime indexes of bitset
+
+	char * toPrint = malloc(1); // sizeof(char) == 1
+	int count = 0; // used to count 8 bits which create a char value
+	int index = 0; // saving newly created chars at toPrint[index]
+	int isNull = 0; // we may print the string only if the NULL char is defined
 
 	for (bitset_index_t i = 23; i<size; i += 2) {
-		// when the bit on index i is 0, it's a prime number
+		// if bitset[i] == 0, then it's a prime number
 		// in that case save the LSB of saved data to toPrint
 		if (!bitset_getbit(primes, i)) {
-//			printf("%c\n", image->data[i]);
-			toPrint[index] |= ((unsigned char)image->data[i] & 0x01) << count;
+			toPrint[index] |= ((unsigned char)image	->data[i] & 0x01) << count;
 			count++;
 		}
 		if (count == 8) {
 //			printf("%c", toPrint);
 			count = 0;
+//			todo: check the error of valgrind here			
 			if (toPrint[index] == '\0') {
-				printf("%s", toPrint);
+				isNull = 1;
 				break;
 			}
 			toPrint = realloc(toPrint, ++index+1);
 			toPrint[index] = '\0';
 		}
 	}
+	// in case of missing '\0' char, exit(1) must be called
+	if (!isNull) {
+		free(toPrint);
+		ppm_free(image);
+		bitset_free(primes);
+		error_exit("Nenalezena tajna zprava, resp. chybi '\0' znak.\n");
+	}
+
+	// in case of found '\0' char, the program ends with success
+	printf("%s", toPrint);
+
 	free(toPrint);
 	ppm_free(image);
 	bitset_free(primes);
