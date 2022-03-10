f9c071afd222653b6497d645bf19b639a9bd8728
smatand <andrejsmatana0@gmail.com>
2022-03-10 12:49:10 +0100

    Refactor code

--- bitset.h
+++ bitset.h
@@ -14,51 +14,58 @@
 typedef unsigned long* bitset_t;
 typedef unsigned long bitset_index_t;
 
+
 #define ULONG               ((sizeof(unsigned long)) * (CHAR_BIT))
 #define MAX_SIZE(velikost)  ((velikost / ULONG) + 1 + ((velikost % ULONG != 0) ? 1 : 0))
 
 /**
  * @brief Get bit from jmeno_pole[index]
+ *
  * @param index which index in array 
  * @return bit value of jmeno_pole[index]
  */
-#define GETBIT(jmeno_pole, index)			(jmeno_pole[(index/ULONG)+1] >> ((index) % ULONG) & 1UL)
+#define GETBIT(jmeno_pole, index)	(jmeno_pole[(index/ULONG)+1] >> ((index) % ULONG) & 1UL)
 
 /**
  * @brief Set bit in bitset
+ *
  * @param jmeno_pole name of bitset
  * @param index as index in bitset array
  * @param vyraz value to which the jmeno_pole[index] will be set
  */
-#define SETBIT(jmeno_pole, index, vyraz)	(vyraz) ? (jmeno_pole[(index/ULONG)+1] |= (1UL << (index % ULONG))) : \
-													  (jmeno_pole[(index/ULONG)+1] &= (1UL << (index % ULONG)))
+#define SETBIT(jmeno_pole, index, vyraz)										\
+	(vyraz) ?	(jmeno_pole[(index/ULONG)+1] |= (1UL << (index % ULONG))) :		\
+				(jmeno_pole[(index/ULONG)+1] &= (1UL << (index % ULONG)))
 
 /**
  * @brief Create bitset jmeno_pole
+ *
  * @param jmeno_pole name of bitset
  * @param velikost size of bitset
  * @return bitset array jmeno_pole
  */
-#define bitset_create(jmeno_pole, velikost)													                   \
-	static_assert(((unsigned long)velikost >= 0), "Velikost pro alokaci bitset_t pole musi byt kladny cislo!");\
+#define bitset_create(jmeno_pole, velikost)	\
+	static_assert(((unsigned long)velikost >= 0), "Velikost pro alokaci bitset_t pole musi byt kladny cislo!");	\
 	unsigned long jmeno_pole[MAX_SIZE(velikost)] = {velikost};	 	 
 
 /**
  * @brief Dynamically allocate bitset
+ *
  * @param jmeno_pole name of bitset
  * @param velikost of size of bitset
  * @return bitset array jmeno_pole
  */
-#define bitset_alloc(jmeno_pole, velikost)									 \
-	assert(!(velikost <= 0));												 \
-	bitset_t jmeno_pole = calloc(MAX_SIZE(velikost), sizeof(unsigned long)); \
-    if (jmeno_pole == NULL) error_exit("bitset_alloc: Chyba alokace pameti");\
+#define bitset_alloc(jmeno_pole, velikost)	\
+	assert(!(velikost <= 0));	\
+	bitset_t jmeno_pole = calloc(MAX_SIZE(velikost), sizeof(unsigned long));	\
+    if (jmeno_pole == NULL) error_exit("bitset_alloc: Chyba alokace pameti");	\
 	jmeno_pole[0] = velikost;
 
 #ifndef USE_INLINE
 
 /**
  * @brief Size of bitset
+ *
  * @param jmeno_pole name of bitset
  * @return size of jmeno_pole array
  */
@@ -66,28 +73,31 @@ typedef unsigned long bitset_index_t;
 
 /**
  * @brief Set bit in bitset
+ *
  * @param jmeno_pole name of bitset
  * @param index as index in bitset array
  * @param vyraz value to which the jmeno_pole[index] will be set
  */
-#define bitset_setbit(jmeno_pole, index, vyraz)                                                        \
-	((index) >= bitset_size(jmeno_pole) ?	(error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu", \
-												(unsigned long)index, bitset_size(jmeno_pole)), 0)   : \
+#define bitset_setbit(jmeno_pole, index, vyraz)	\
+	((index) >= bitset_size(jmeno_pole) ?	(error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu",	\
+												(unsigned long)index, bitset_size(jmeno_pole)), 0)   :	\
 										    (SETBIT(jmeno_pole, index, vyraz)))
 
 /**
  * @brief Get bit from jmeno_pole[index]
+ *
  * @param jmeno_pole name of bitset
  * @param index as index in bitset array
  * @return value of jmeno_pole[index]
  */
-#define bitset_getbit(jmeno_pole, index)																			  \
-	((index) >= bitset_size(jmeno_pole) ?   (error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu",				  \
-												(unsigned long)index, (unsigned long)bitset_size(jmeno_pole)), 0)   : \
+#define bitset_getbit(jmeno_pole, index)	\
+	((index) >= bitset_size(jmeno_pole) ?	\
+		(error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu", (unsigned long)index, (unsigned long)bitset_size(jmeno_pole)), 0)	:	\
 											(GETBIT(jmeno_pole, index)))
 
 /**
  * @brief Free dynamically allocated bitset array
+ *
  * @param jmeno_pole name of bitset
  */
 #define bitset_free(jmeno_pole) free(jmeno_pole)
@@ -96,6 +106,7 @@ typedef unsigned long bitset_index_t;
 
 /**
  * @brief Size of bitset
+ *
  * @param jmeno_pole of name of bitset
  * @return size of jmeno_pole array
  */
@@ -105,6 +116,7 @@ inline unsigned long bitset_size(bitset_t jmeno_pole) {
 
 /**
  * @brief Set bit in bitset
+ *
  * @param jmeno_pole name of bitset
  * @param index as index in bitset array
  * @param vyraz value to which the jmeno_pole[index] will be set
@@ -118,6 +130,7 @@ inline void bitset_setbit(bitset_t jmeno_pole, const unsigned long index, const 
 
 /**
  * @brief Get bit from jmeno_pole[index]
+ *
  * @param jmeno_pole name of bitset
  * @param index as index in bitset array
  * @return value of jmeno_pole[index]
@@ -131,6 +144,7 @@ inline unsigned long bitset_getbit(bitset_t jmeno_pole, const unsigned long inde
 
 /**
  * @brief Free dynamically allocated bitset array
+ *
  * @param jmeno_pole name of bitset
  */
 inline void bitset_free(bitset_t jmeno_pole) {
